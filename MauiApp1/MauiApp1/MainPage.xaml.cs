0925a633df1935e8f4225e3613c80e3d655df19d
Rodion Skorba <r.skorba@mail.ru>
2023-05-06 16:52:07 +0500

    Test commit feat: no feat

--- MauiApp1/MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -2,9 +2,30 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    async void OnButtonClicked(object sender, EventArgs args)
+    {
+        //string text = OnEntryCompleted(sender, args);
+
+    }
+
+    void OnEntryCompleted(object sender, EventArgs e)
+    {
+        string text = ((Entry)sender).Text;
+        //return text;
+    }
+
+    void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        //string oldText = e.OldTextValue;
+        //string newText = e.NewTextValue;
+        //string myText = entry.Text;
+    }
 }
+
+
 
