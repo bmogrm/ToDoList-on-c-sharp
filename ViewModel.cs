6c3e86c741485bf838a4384cfca0961825930b4b
Rodion Skorba <r.skorba@mail.ru>
2023-05-16 12:27:33 +0500

    Fix: Cell size in CollectionView in Xaml

---
+++ ViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using MauiApp1.Model;
+
+namespace MauiApp1.ViewModel
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand createTask { get; }
+        public ICommand deleteTask { get; set; }
+        public ICommand changeCategory { get; set; }
+        public ICommand changeSelect { get; }
+
+        public ObservableCollection<Model.Model> ListTasks { get; set; } =
+        new ObservableCollection<Model.Model>();
+
+        public Model.Model task;
+        public Color colCategory;
+
+        private void Create(string entry)
+        {
+            ListTasks.Add(new Model.Model() {TaskName = entry, Status = false, Category = colCategory});
+            colCategory = null;
+            ColButton = Color.FromHex("#512bd4");
+        }
+
+        private void Delete(Model.Model selTask) => ListTasks.Remove(selTask);
+
+        private void ChangeCategory(string text)
+        {
+            ColButton = text switch
+            {
+                "red" => Color.FromHex("#b81111"),
+                "green" => Color.FromHex("#8cb42f"),
+                "blue" => Color.FromHex("#499fdf"),
+                _ => ColButton
+            };
+        }
+
+        public Color ColButton
+        {
+            get => colCategory;
+            set
+            {
+                colCategory = value;
+                OnPropertyChanged(nameof(ColButton));
+            }
+        }
+
+        public async void EditTask()
+        {
+            string switchCat = await Application.Current.MainPage.DisplayActionSheet("Выберите действие", "Отмена", "", "Сменить название", "Красная категория", "Зеленая категория", "Голубая категория");
+            switch(switchCat)
+            {
+                case "Сменить название":
+                    string text = await Application.Current.MainPage.DisplayPromptAsync("Редактирование", "Введите новое название задачи", initialValue: selectedTask.TaskName);
+                    selectedTask.TaskName = text;
+                    break;
+                case "Красная категория":
+                    selectedTask.Category = Color.FromHex("#b81111");
+                    break;
+                case "Зеленая категория":
+                    selectedTask.Category = Color.FromHex("#8cb42f");
+                    break;
+                case "Голубая категория":
+                    selectedTask.Category = Color.FromHex("#499fdf");
+                    break;
+                default:
+                    selectedTask = null;
+                    break;
+            }
+            selectedTask = null;
+        }
+
+        public Model.Model selectedTask
+        {
+            get => task;
+            set
+            {
+                if (task != value)
+                {
+                    task = value;
+                    if (task != null)
+                        EditTask();
+                }
+            OnPropertyChanged(nameof(selectedTask));
+            }
+        }
+
+        public ViewModel()
+        {
+            task = new Model.Model();
+            createTask = new Command<string>(Create);
+            deleteTask = new Command<Model.Model>(Delete);
+            changeCategory = new Command<string>(ChangeCategory);
+            changeSelect = new Command<Model.Model>(x=>selectedTask=x);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
